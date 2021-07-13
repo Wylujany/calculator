c019299cb0ff743ad02ccc1b09370dad65012463
Wiktor <gejzowany@gmail.com>
2021-07-13 22:47:04 +0200

    Can do operations on single digit inputs

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 const display = document.querySelector("#display");
-const buttons = document.querySelectorAll(".number");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+let displayValue;
+let firstNumber;
+let operator;
+let secondNumber;
 
-buttons.forEach((button) => {
+numberButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    display.textContent += e.target.textContent;
+    if (!firstNumber) {
+      display.textContent += e.target.textContent;
+      displayValue = display.textContent;
+      firstNumber = parseFloat(e.target.textContent);
+    } else {
+      display.textContent = e.target.textContent;
+      displayValue = display.textContent;
+      secondNumber = parseFloat(display.textContent);
+    }
   });
 });
 
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    display.textContent = e.target.textContent;
+    operator = display.textContent;
+  });
+});
+function resolve() {
+  operate(operator, firstNumber, secondNumber);
+}
 function operate(operator, firstNumber, secondNumber) {
   if (operator == "+") {
-    add(fistNumber, secondNumber);
+    add(firstNumber, secondNumber);
   } else if (operator == "-") {
-    subtract(fistNumber, secondNumber);
+    subtract(firstNumber, secondNumber);
   } else if (operator == "*") {
-    multiply(fistNumber, secondNumber);
+    multiply(firstNumber, secondNumber);
   } else if (operator == "/") {
-    divide(fistNumber, secondNumber);
+    divide(firstNumber, secondNumber);
   }
+}
+
+function add(a, b) {
+  return (display.textContent = a + b);
+}
+function subtract(a, b) {
+  return (display.textContent = a - b);
+}
+
+function multiply(a, b) {
+  return (display.textContent = a * b);
+}
+
+function divide(a, b) {
+  return (display.textContent = a / b);
 }
