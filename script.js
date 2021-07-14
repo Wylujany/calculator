a32d246cf4bffecf67c9787c2313ffbf3a13d043
Wiktor <gejzowany@gmail.com>
2021-07-14 15:16:41 +0200

    Final commits

--- script.js
+++ script.js
@@ -1,76 +1,108 @@
-const display = document.querySelector("#display");
+const displaySolution = document.querySelector("#displaySolution");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
-const clearButton = document.querySelector("#clear");
-let displayValue;
-let firstNumber = 0;
-let operator;
-let secondNumber = 0;
-clearButton.addEventListener("click", () => clear());
+const clearButton = document.querySelector(".clear");
+const backspaceButton = document.querySelector(".backspace");
+const equalsButton = document.querySelector(".equals");
+const decimalButton = document.querySelector(".decimal");
 
-function clear() {
-  display.textContent = "";
-  firstNumber = 0;
-  secondNumber = 0;
-  operator = "";
-  console.log(firstNumber);
-}
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = null;
+let shouldResetDisplay = false;
+
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+backspaceButton.addEventListener("click", backspace);
+decimalButton.addEventListener("click", appendPoint);
 
 numberButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (!operator) {
-      display.textContent += e.target.textContent;
-      displayValue = display.textContent;
-      firstNumber = parseFloat(displayValue);
-      console.log(firstNumber);
-    } else {
-      display.textContent += e.target.textContent;
-      displayValue = display.textContent;
-      secondNumber = parseFloat(display.textContent);
-      console.log(secondNumber);
-    }
-  });
+  button.addEventListener("click", () => appendNumber(button.textContent));
 });
 
 operatorButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    display.textContent = e.target.textContent;
-    operator = display.textContent;
-    display.textContent = "";
-    console.log(firstNumber);
-    console.log(secondNumber);
-    console.log(operator);
-  });
+  button.addEventListener("click", () => setOperation(button.textContent));
 });
 
-function resolve() {
-  operate(operator, firstNumber, secondNumber);
+function appendNumber(num) {
+  if (displaySolution === "0" || shouldResetDisplay) resetDisplay();
+  displaySolution.textContent += num;
 }
 
-function operate(operator, firstNumber, secondNumber) {
-  if (operator == "+") {
-    add(firstNumber, secondNumber);
-  } else if (operator == "-") {
-    subtract(firstNumber, secondNumber);
-  } else if (operator == "*") {
-    multiply(firstNumber, secondNumber);
-  } else if (operator == "/") {
-    divide(firstNumber, secondNumber);
+function resetDisplay() {
+  displaySolution.textContent = "";
+  shouldResetDisplay = false;
+}
+
+function clear() {
+  displaySolution.textContent = "0";
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = null;
+}
+
+function appendPoint() {
+  if (shouldResetDisplay) resetDisplay();
+  if (displaySolution.textContent === "") displaySolution.textContent = "0";
+  if (displaySolution.textContent.includes(".")) return;
+  displaySolution.textContent += ".";
+}
+
+function backspace() {
+  displaySolution.textContent = displaySolution.textContent
+    .toString()
+    .slice(0, -1);
+}
+
+function setOperation(operator) {
+  if (currentOperator !== null) evaluate();
+  firstNumber = displaySolution.textContent;
+  currentOperator = operator;
+  shouldResetDisplay = true;
+}
+
+function evaluate() {
+  if (currentOperator === null || shouldResetDisplay) return;
+  if (currentOperator === "/" && screen.textContent === "0") {
+    alert("You cant divide by 0!");
+    clear();
+    return;
+  }
+  secondNumber = displaySolution.textContent;
+  displaySolution.textContent = roundNumbers(
+    operate(currentOperator, firstNumber, secondNumber)
+  );
+  currentOperator = null;
+}
+function roundNumbers(num) {
+  return Math.round(num * 10) / 10;
+}
+function operate(operator, first, second) {
+  first = Number(first);
+  second = Number(second);
+  switch (operator) {
+    case "+":
+      return add(first, second);
+    case "-":
+      return subtract(first, second);
+    case "*":
+      return multiply(first, second);
+    case "/":
+      return divide(first, second);
+    default:
+      return null;
   }
 }
 
 function add(a, b) {
-  return (display.textContent = a + b);
+  return a + b;
 }
-
 function subtract(a, b) {
-  return (display.textContent = a - b);
+  return a - b;
 }
-
 function multiply(a, b) {
-  return (display.textContent = a * b);
+  return a * b;
 }
-
 function divide(a, b) {
-  return (display.textContent = a / b);
+  return a / b;
 }
